6b0e105d225fcf3c232d39379cc62e9273eb1fbf
multicode <mr.mirmakhmudov16112008@gmail.com>
2025-07-08 00:51:06 +0500

    Commit first template

---
+++ cerifynow/settings.py
@@ -0,0 +1,175 @@
+import os
+from pathlib import Path
+from config import get_settings
+
+env = get_settings()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = env.secret_key
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DJANGO_APPS = [
+    'jazzmin',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+THIRD_PARTY_APPS = [
+
+]
+
+LOCAL_APPS = [
+    'institutions',
+    'users'
+]
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'cerifynow.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'cerifynow.wsgi.application'
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.db_name,
+        "USER": env.db_user,
+        "PASSWORD": env.db_password,
+        "HOST": env.db_host,
+        "PORT": env.db_port,
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Tashkent'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    "site_header": "Administration",
+
+    "site_brand": "Metsenat API",
+
+    "site_logo": "../media/avatar/student/default-student.webp",
+
+    "welcome_sign": "Welcome to our Metsenat-API",
+
+    "search_model": ["auth.User", "apps.Product"],
+
+    # applarni yashirish uchun
+    "hide_apps": [],
+
+    # bazi modellarni yashirish uchun
+    "hide_models": ['auth.Group'],
+
+    # tepa menu da home va userlar ni chiqarish uchun
+    "topmenu_links": [
+        # Url that gets reversed (Permissions
+        {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
+        {"model": "auth.User"},
+    ],
+    # user iconka sin chiqarish uchun
+    "usermenu_links": [
+        {"name": "Support", "url": "https://github.com/theMirmakhmudov", "new_window": True},
+        # {"model": "auth.user"},
+    ],
+    "copyright": "Mr.Mirmakhmudov",
+    "language_chooser": False,
+    "show_ui_builder": True,
+    # "language_chooser": True,
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fa fa-layer-group",
+    'icons': {},
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": True,
+    "footer_small_text": True,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-gray",
+    "accent": "accent-info",
+    "navbar": "navbar-gray-dark navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-info",
+    "sidebar_nav_small_text": True,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "darkly",
+    "dark_mode_theme": "cyborg",
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    },
+    "actions_sticky_top": False
+}
